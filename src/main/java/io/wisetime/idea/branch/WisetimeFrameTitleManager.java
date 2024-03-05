eec491e90f216ec21bc90c86f8ff6c24d379225c
jiabao.gao <jiabao.gao@zhaopin.com.cn>
2024-03-05 20:32:45 +0800

    update to 1.1.1: 默认使用项目根目录文件夹名作为项目名; 方便在本地对项目进行重命名.

--- src/main/java/io/wisetime/idea/branch/WisetimeFrameTitleManager.java
+++ src/main/java/io/wisetime/idea/branch/WisetimeFrameTitleManager.java
@@ -1,8 +1,11 @@
 package io.wisetime.idea.branch;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.io.FileUtil;
 import com.intellij.openapi.wm.impl.PlatformFrameTitleBuilder;
+
 import java.util.Optional;
+
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -11,31 +14,48 @@ import org.jetbrains.annotations.NotNull;
  */
 public class WisetimeFrameTitleManager extends PlatformFrameTitleBuilder {
 
-  /**
-   * Window title format pre IntelliJ IDEA 2020.1.
-   */
-  private static final String WINDOW_TITLE_BRACKETS_PATTERN = "^(.*?)\\[.+?](.*)";
+    /**
+     * Window title format pre IntelliJ IDEA 2020.1.
+     */
+    private static final String WINDOW_TITLE_BRACKETS_PATTERN = "^(.*?)\\[.+?](.*)";
 
-  @Override
-  public String getProjectTitle(@NotNull Project project) {
-    final String currentBranch = Optional.ofNullable(project.getService(BranchHelper.class))
-        .map(BranchHelper::getCurrentBranchName)
-        // return empty string if branch name is unavailable
-        .orElse("");
-    return getProjectTitleWithBranch(project, currentBranch);
-  }
-
-  String getProjectTitleWithBranch(Project project, String branchName) {
-    String projectTitle = super.getProjectTitle(project);
-    if (branchName != null && !branchName.isEmpty()) {
-      if (projectTitle.matches(WINDOW_TITLE_BRACKETS_PATTERN)) {
-        // if an existing title ends with context data inside square brackets, replace with the updated branch title
-        projectTitle = projectTitle.replaceAll(WINDOW_TITLE_BRACKETS_PATTERN, "$1[" + branchName + "]$2");
-      } else {
-        projectTitle += " [" + branchName + "]";
-      }
+    @Override
+    public String getProjectTitle(@NotNull Project project) {
+        final String currentBranch = Optional.ofNullable(project.getService(BranchHelper.class))
+                .map(BranchHelper::getCurrentBranchName)
+                // return empty string if branch name is unavailable
+                .orElse("");
+        return getProjectTitleWithBranch(project, currentBranch);
     }
-    return projectTitle;
-  }
+
+    String getProjectTitleWithBranch(Project project, String branchName) {
+        String projectTitle = getProjectDisplayName(project);
+        if (branchName != null && !branchName.isEmpty()) {
+            if (projectTitle.matches(WINDOW_TITLE_BRACKETS_PATTERN)) {
+                // if an existing title ends with context data inside square brackets, replace with the updated branch title
+                projectTitle = projectTitle.replaceAll(WINDOW_TITLE_BRACKETS_PATTERN, "$1[" + branchName + "]$2");
+            } else {
+                projectTitle += " [" + branchName + "]";
+            }
+        }
+        return projectTitle;
+    }
+
+    /**
+     * 优先使用项目文件夹名字.这样可以通过重命名项目文件夹来重命名.
+     */
+    private String getProjectDisplayName(Project project) {
+        String basePath = project.getBasePath();
+        if (basePath == null) {
+            return project.getName();
+        }
+        final String[] splits = basePath.split("/");
+        if (splits.length == 0) {
+            return project.getName();
+        }
+
+        final String projectLeafDir = splits[splits.length - 1];
+        return projectLeafDir;
+    }
 
 }
